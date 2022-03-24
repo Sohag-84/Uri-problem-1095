37dc4cd1f615b137345716fde4467dc974bb1f33
Injamul Haq Sohag <99319134+Sohag-84@users.noreply.github.com>
2022-03-24 18:56:23 +0600

    Add files via upload

---
+++ Problem_1095.java
@@ -0,0 +1,12 @@
+package Uri_online;
+
+public class Problem_1095 {
+    public static void main(String[] args) {
+        int i=1;
+        int j;
+        for (j = 60; j >=0; j=j-5) {
+            System.out.println("I="+i+" J="+j);
+            i=i+3;
+        }
+    }
+}
